1af47572f689cabe6039e237366b2c69577402ed
caisl <caishenglang@foxmail.com>
2020-05-25 00:16:57 +0800

    Optimize image utils

--- app/src/main/java/com/dylanc/utilktx/sample/MainActivity.kt
+++ app/src/main/java/com/dylanc/utilktx/sample/MainActivity.kt
@@ -2,11 +2,24 @@ package com.dylanc.utilktx.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.blankj.utilcode.util.MapUtils
+import com.dylanc.utilktx.*
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    tv.setShadow {
+      radius(8.dp.toFloat())
+      size(4.dp)
+    }
+
+    notify(1) {
+      setSmallIcon(R.mipmap.ic_launcher)
+      setContentText("klasdja jaslkjdklasjdlkajsdlkjal k")
+    }
   }
 }
