12778a1fbccb3f4c29f03f441fe7dade7b811a11
caisl <caishenglang@foxmail.com>
2020-07-11 23:42:35 +0800

    Optimize CleanUtils and CloneUtils

--- library/src/main/java/com/dylanc/utilktx/CleanUtils.kt
+++ library/src/main/java/com/dylanc/utilktx/CleanUtils.kt
@@ -1,6 +1,28 @@
 package com.dylanc.utilktx
 
+import com.blankj.utilcode.util.CleanUtils
+
 /**
  * @author Dylan Cai
  * @since 2020/5/9
  */
+fun cleanInternalCache() =
+  CleanUtils.cleanInternalCache()
+
+fun cleanInternalFiles() =
+  CleanUtils.cleanInternalFiles()
+
+fun cleanInternalDbs() =
+  CleanUtils.cleanInternalDbs()
+
+fun cleanInternalDbByName(dbName: String) =
+  CleanUtils.cleanInternalDbByName(dbName)
+
+fun cleanInternalSp() =
+  CleanUtils.cleanInternalSp()
+
+fun cleanExternalCache() =
+  CleanUtils.cleanExternalCache()
+
+fun cleanCustomDir(dirPath: String) =
+  CleanUtils.cleanCustomDir(dirPath)
--- library/src/main/java/com/dylanc/utilktx/CloneUtils.kt
+++ library/src/main/java/com/dylanc/utilktx/CloneUtils.kt
@@ -1,6 +1,14 @@
 package com.dylanc.utilktx
 
+import com.blankj.utilcode.util.CloneUtils
+import java.lang.reflect.Type
+
 /**
  * @author Dylan Cai
  * @since 2020/5/9
  */
+inline fun <reified T> T.deepClone(): T =
+  CloneUtils.deepClone(this, typeOf(T::class.java))
+
+fun <T> T.deepClone(type: Type): T =
+  CloneUtils.deepClone(this, type)
