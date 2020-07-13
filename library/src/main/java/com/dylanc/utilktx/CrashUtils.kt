0d3aed304767ee1b301c52cefd560d7f6552790f
caisl <caishenglang@foxmail.com>
2020-07-13 23:40:11 +0800

    Optimize CrashUtils and FlashlightUtils

--- library/src/main/java/com/dylanc/utilktx/CrashUtils.kt
+++ library/src/main/java/com/dylanc/utilktx/CrashUtils.kt
@@ -1,6 +1,14 @@
 package com.dylanc.utilktx
 
+import com.blankj.utilcode.util.CrashUtils
+import java.io.File
+
 /**
  * @author Dylan Cai
  * @since 2020/5/9
  */
+fun observeCrash(crashDir: File, listener: (String, Throwable) -> Unit) =
+  CrashUtils.init(crashDir,listener)
+
+fun observeCrash(crashDirPath: String = "", listener: (String, Throwable) -> Unit) =
+  CrashUtils.init(crashDirPath, listener)
--- library/src/main/java/com/dylanc/utilktx/FlashlightUtils.kt
+++ library/src/main/java/com/dylanc/utilktx/FlashlightUtils.kt
@@ -1,6 +1,20 @@
 package com.dylanc.utilktx
 
+import com.blankj.utilcode.util.FlashlightUtils
+
 /**
  * @author Dylan Cai
  * @since 2020/5/15
  */
+
+val isFlashlightEnable: Boolean
+  get() = FlashlightUtils.isFlashlightEnable()
+
+val isFlashlightOn: Boolean
+  get() = FlashlightUtils.isFlashlightOn()
+
+fun setFlashlightStatus(isOn: Boolean) =
+  FlashlightUtils.setFlashlightStatus(isOn)
+
+fun destroyFlashlight() =
+  FlashlightUtils.destroy()
