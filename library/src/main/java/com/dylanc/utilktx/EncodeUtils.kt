d06b2f2430df01cecbc0329f37b49edc98598b99
caisl <caishenglang@foxmail.com>
2020-07-14 23:43:14 +0800

    Optimize EncodeUtils and FileIOUtils

--- library/src/main/java/com/dylanc/utilktx/EncodeUtils.kt
+++ library/src/main/java/com/dylanc/utilktx/EncodeUtils.kt
@@ -1,6 +1,40 @@
 package com.dylanc.utilktx
 
+import com.blankj.utilcode.util.EncodeUtils
+
 /**
  * @author Dylan Cai
  * @since 2020/5/15
  */
+fun String.urlEncode(charsetName: String = "UTF-8"): String =
+  EncodeUtils.urlEncode(this, charsetName)
+
+fun String.urlDecode(charsetName: String = "UTF-8"): String =
+  EncodeUtils.urlDecode(this, charsetName)
+
+fun String.base64Encode(): ByteArray =
+  EncodeUtils.base64Encode(this)
+
+fun ByteArray.base64Encode(): ByteArray =
+  EncodeUtils.base64Encode(this)
+
+fun String.base64Decode(): ByteArray =
+  EncodeUtils.base64Decode(this)
+
+fun ByteArray.base64Decode(): ByteArray =
+  EncodeUtils.base64Decode(this)
+
+fun ByteArray.base64Encode2String(): String =
+  EncodeUtils.base64Encode2String(this)
+
+fun CharSequence.htmlEncode(): String =
+  EncodeUtils.htmlEncode(this)
+
+fun String.htmlDecode(): CharSequence =
+  EncodeUtils.htmlDecode(this)
+
+fun String.binaryEncode(): String =
+  EncodeUtils.binaryEncode(this)
+
+fun String.binaryDecode(): String =
+  EncodeUtils.binaryDecode(this)
--- library/src/main/java/com/dylanc/utilktx/FileIOUtils.kt
+++ library/src/main/java/com/dylanc/utilktx/FileIOUtils.kt
@@ -1,6 +1,80 @@
 package com.dylanc.utilktx
 
+import com.blankj.utilcode.util.FileIOUtils
+import java.io.File
+import java.io.InputStream
+
 /**
  * @author Dylan Cai
  * @since 2020/5/15
  */
+fun InputStream.writeFile(
+  filePath: String,
+  append: Boolean = false,
+  listener: ((Double) -> Unit)? = null
+): Boolean =
+  FileIOUtils.writeFileFromIS(filePath, this, append, listener)
+
+fun InputStream.writeFile(
+  file: File,
+  append: Boolean = false,
+  listener: ((Double) -> Unit)? = null
+): Boolean =
+  FileIOUtils.writeFileFromIS(file, this, append, listener)
+
+fun ByteArray.writeFile(
+  filePath: String,
+  append: Boolean = false,
+  listener: ((Double) -> Unit)? = null
+): Boolean =
+  FileIOUtils.writeFileFromBytesByStream(filePath, this, append, listener)
+
+fun ByteArray.writeFile(
+  file: File,
+  append: Boolean = false,
+  listener: ((Double) -> Unit)? = null
+): Boolean =
+  FileIOUtils.writeFileFromBytesByStream(file, this, append, listener)
+
+fun ByteArray.writeFileByChannel(
+  filePath: String,
+  isForce: Boolean,
+  append: Boolean = false
+): Boolean =
+  FileIOUtils.writeFileFromBytesByChannel(filePath, this, append, isForce)
+
+fun ByteArray.writeFileByChannel(
+  file: File,
+  isForce: Boolean,
+  append: Boolean = false
+): Boolean =
+  FileIOUtils.writeFileFromBytesByChannel(file, this, append, isForce)
+
+fun ByteArray.writeFileByMap(
+  filePath: String,
+  isForce: Boolean,
+  append: Boolean = false
+): Boolean =
+  FileIOUtils.writeFileFromBytesByMap(filePath, this, append, isForce)
+
+fun ByteArray.writeFileByMap(
+  file: File,
+  isForce: Boolean,
+  append: Boolean = false
+): Boolean =
+  FileIOUtils.writeFileFromBytesByMap(file, this, append, isForce)
+
+fun String.writeFile(
+  filePath: String,
+  append: Boolean = false
+): Boolean =
+  FileIOUtils.writeFileFromString(filePath, this, append)
+
+fun String.writeFile(
+  file: File,
+  append: Boolean = false
+): Boolean =
+  FileIOUtils.writeFileFromString(file, this, append)
+
+//fun readFile() =
+//  FileIOUtils.readFile2List()
