3ca6e11710dfc950f8f3e3cdf77938dec13e0cda
dylanc <caishenglang@foxmail.com>
2020-08-04 17:56:04 +0800

    Add comments in AppUtils and BarUtils

--- library/src/main/java/com/dylanc/utilktx/AppUtils.kt
+++ library/src/main/java/com/dylanc/utilktx/AppUtils.kt
@@ -2,9 +2,14 @@
 
 package com.dylanc.utilktx
 
+import android.Manifest.permission.REQUEST_DELETE_PACKAGES
+import android.Manifest.permission.REQUEST_INSTALL_PACKAGES
 import android.app.Application
 import android.content.pm.Signature
 import android.graphics.drawable.Drawable
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.annotation.RequiresPermission
 import com.blankj.utilcode.util.AppUtils
 import com.blankj.utilcode.util.Utils
 import java.io.File
@@ -13,131 +18,359 @@ import java.io.File
  * @author Dylan Cai
  */
 
-inline val application: Application
-  get() = Utils.getApp()
+/**
+ * Return the Application object. Main process get app by UtilsFileProvider,
+ * and other process get app by reflect.. This is equivalent to calling:
+ * ```
+ * Utils.getApp()
+ * ```
+ */
+inline val application: Application get() = Utils.getApp()
 
+/**
+ * Register the status of application changed listener. This is equivalent to calling:
+ * ```
+ * AppUtils.registerAppStatusChangedListener(listener)
+ * ```
+ */
 inline fun registerAppStatusChangedListener(listener: Utils.OnAppStatusChangedListener) =
   AppUtils.registerAppStatusChangedListener(listener)
 
+/**
+ * Unregister the status of application changed listener. This is equivalent to calling:
+ * ```
+ * AppUtils.unregisterAppStatusChangedListener(listener)
+ * ```
+ */
 inline fun unregisterAppStatusChangedListener(listener: Utils.OnAppStatusChangedListener) =
   AppUtils.unregisterAppStatusChangedListener(listener)
 
-inline fun installApp(file: File) =
-  AppUtils.installApp(file)
+/**
+ * Install the app. This is equivalent to calling:
+ * ```
+ * AppUtils.installApp(file)
+ * ```
+ */
+@RequiresApi(Build.VERSION_CODES.M)
+@RequiresPermission(REQUEST_INSTALL_PACKAGES)
+inline fun installApp(file: File) = AppUtils.installApp(file)
 
-inline fun installApp(filePath: String) =
-  AppUtils.installApp(filePath)
+/**
+ * Install the app. This is equivalent to calling:
+ * ```
+ * AppUtils.installApp(filePath)
+ * ```
+ */
+@RequiresApi(Build.VERSION_CODES.M)
+@RequiresPermission(REQUEST_INSTALL_PACKAGES)
+inline fun installApp(filePath: String) = AppUtils.installApp(filePath)
 
-inline fun uninstallApp(packageName: String) =
-  AppUtils.uninstallApp(packageName)
+/**
+ * Uninstall the app. This is equivalent to calling:
+ * ```
+ * AppUtils.uninstallApp(packageName)
+ * ```
+ */
+@RequiresApi(Build.VERSION_CODES.O)
+@RequiresPermission(REQUEST_DELETE_PACKAGES)
+inline fun uninstallApp(packageName: String) = AppUtils.uninstallApp(packageName)
 
-inline fun isAppInstalled(packageName: String): Boolean =
-  AppUtils.isAppInstalled(packageName)
+/**
+ * Return whether the app is installed. This is equivalent to calling:
+ * ```
+ * AppUtils.isAppInstalled(packageName)
+ * ```
+ */
+inline fun isAppInstalled(packageName: String): Boolean = AppUtils.isAppInstalled(packageName)
 
-inline val isAppRoot: Boolean
-  get() = AppUtils.isAppRoot()
+/**
+ * Return whether the application with root permission. This is equivalent to calling:
+ * ```
+ * AppUtils.isAppRoot()
+ * ```
+ */
+inline val isAppRoot: Boolean get() = AppUtils.isAppRoot()
 
-inline val isAppDebug: Boolean
-  get() = AppUtils.isAppDebug()
+/**
+ * Return whether it is a debug application. This is equivalent to calling:
+ * ```
+ * AppUtils.isAppDebug()
+ * ```
+ */
+inline val isAppDebug: Boolean get() = AppUtils.isAppDebug()
 
-inline fun isAppDebug(packageName: String): Boolean =
-  AppUtils.isAppDebug(packageName)
+/**
+ * Return whether it is a debug application. This is equivalent to calling:
+ * ```
+ * AppUtils.isAppDebug(packageName)
+ * ```
+ */
+inline fun isAppDebug(packageName: String): Boolean = AppUtils.isAppDebug(packageName)
 
-inline val isAppSystem: Boolean
-  get() = AppUtils.isAppSystem()
+/**
+ * Return whether it is a system application. This is equivalent to calling:
+ * ```
+ * AppUtils.isAppSystem()
+ * ```
+ */
+inline val isAppSystem: Boolean get() = AppUtils.isAppSystem()
 
-inline fun isAppSystem(packageName: String): Boolean =
-  AppUtils.isAppSystem(packageName)
+/**
+ * Return whether it is a system application. This is equivalent to calling:
+ * ```
+ * AppUtils.isAppSystem(packageName)
+ * ```
+ */
+inline fun isAppSystem(packageName: String): Boolean = AppUtils.isAppSystem(packageName)
 
-inline val isAppForeground: Boolean
-  get() = AppUtils.isAppForeground()
+/**
+ * Return whether application is foreground. This is equivalent to calling:
+ * ```
+ * AppUtils.isAppForeground()
+ * ```
+ */
+inline val isAppForeground: Boolean get() = AppUtils.isAppForeground()
 
-inline fun isAppForeground(packageName: String): Boolean =
-  AppUtils.isAppForeground(packageName)
+/**
+ * Return whether application is foreground. This is equivalent to calling:
+ * ```
+ * AppUtils.isAppForeground(packageName)
+ * ```
+ */
+inline fun isAppForeground(packageName: String): Boolean = AppUtils.isAppForeground(packageName)
 
-inline fun isAppRunning(packageName: String): Boolean =
-  AppUtils.isAppRunning(packageName)
+/**
+ * Return whether application is running. This is equivalent to calling:
+ * ```
+ * AppUtils.isAppRunning(packageName)
+ * ```
+ */
+inline fun isAppRunning(packageName: String): Boolean = AppUtils.isAppRunning(packageName)
 
-inline fun launchApp(packageName: String) =
-  AppUtils.launchApp(packageName)
+/**
+ * Launch the application. This is equivalent to calling:
+ * ```
+ * AppUtils.launchApp(packageName)
+ * ```
+ */
+inline fun launchApp(packageName: String) = AppUtils.launchApp(packageName)
 
-inline fun relaunchApp(isKillProcess: Boolean = false) =
-  AppUtils.relaunchApp(isKillProcess)
+/**
+ * Relaunch the application. This is equivalent to calling:
+ * ```
+ * AppUtils.relaunchApp(isKillProcess)
+ * ```
+ */
+inline fun relaunchApp(isKillProcess: Boolean = false) = AppUtils.relaunchApp(isKillProcess)
 
+/**
+ * Launch the details settings of application. This is equivalent to calling:
+ * ```
+ * AppUtils.launchAppDetailsSettings(packageName)
+ * ```
+ */
 inline fun launchAppDetailsSettings(packageName: String) =
   AppUtils.launchAppDetailsSettings(packageName)
 
-inline fun exitApp() =
-  AppUtils.exitApp()
+/**
+ * Exit the application. This is equivalent to calling:
+ * ```
+ * AppUtils.exitApp()
+ * ```
+ */
+inline fun exitApp() = AppUtils.exitApp()
 
-inline val appIcon: Drawable
-  get() = AppUtils.getAppIcon()
+/**
+ * Return the icon of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppIcon()
+ * ```
+ */
+inline val appIcon: Drawable get() = AppUtils.getAppIcon()
 
-inline fun appIconOf(packageName: String): Drawable =
-  AppUtils.getAppIcon(packageName)
+/**
+ * Return the icon of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppIcon(packageName)
+ * ```
+ */
+inline fun appIconOf(packageName: String): Drawable = AppUtils.getAppIcon(packageName)
 
-inline val packageName: String
-  get() = AppUtils.getAppPackageName()
+/**
+ * Return the package name of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppPackageName()
+ * ```
+ */
+inline val packageName: String get() = AppUtils.getAppPackageName()
 
-inline val appName: String
-  get() = AppUtils.getAppName()
+/**
+ * Return the name of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppName()
+ * ```
+ */
+inline val appName: String get() = AppUtils.getAppName()
 
-inline fun appNameOf(packageName: String): String =
-  AppUtils.getAppName(packageName)
+/**
+ * Return the name of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppName(packageName)
+ * ```
+ */
+inline fun appNameOf(packageName: String): String = AppUtils.getAppName(packageName)
 
-inline val appPath: String
-  get() = AppUtils.getAppPath()
+/**
+ * Return the path of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppPath()
+ * ```
+ */
+inline val appPath: String get() = AppUtils.getAppPath()
 
-inline fun appPathOf(packageName: String): String =
-  AppUtils.getAppPath(packageName)
+/**
+ * Return the path of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppPath(packageName)
+ * ```
+ */
+inline fun appPathOf(packageName: String): String = AppUtils.getAppPath(packageName)
 
-inline val appVersionName: String
-  get() = AppUtils.getAppVersionName()
+/**
+ * Return the version name of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppVersionName()
+ * ```
+ */
+inline val appVersionName: String get() = AppUtils.getAppVersionName()
 
-inline fun appVersionNameOf(packageName: String): String =
-  AppUtils.getAppVersionName(packageName)
+/**
+ * Return the version name of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppVersionName(packageName)
+ * ```
+ */
+inline fun appVersionNameOf(packageName: String): String = AppUtils.getAppVersionName(packageName)
 
-inline val appVersionCode: Int
-  get() = AppUtils.getAppVersionCode()
+/**
+ * Return the version code of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppVersionCode()
+ * ```
+ */
+inline val appVersionCode: Int get() = AppUtils.getAppVersionCode()
 
-inline fun appVersionCodeOf(packageName: String): Int =
-  AppUtils.getAppVersionCode(packageName)
+/**
+ * Return the version code of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppVersionCode(packageName)
+ * ```
+ */
+inline fun appVersionCodeOf(packageName: String): Int = AppUtils.getAppVersionCode(packageName)
 
-inline val appSignature: Array<Signature>
-  get() = AppUtils.getAppSignature()
+/**
+ * Return the signature of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppSignature()
+ * ```
+ */
+inline val appSignature: Array<Signature> get() = AppUtils.getAppSignature()
 
+/**
+ * Return the signature of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppSignature(packageName)
+ * ```
+ */
 inline fun appSignatureOf(packageName: String): Array<Signature> =
   AppUtils.getAppSignature(packageName)
 
-inline val appSignatureSHA1: String
-  get() = AppUtils.getAppSignatureSHA1()
+/**
+ * Return the signature of application for SHA1 value. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppSignatureSHA1()
+ * ```
+ */
+inline val appSignatureSHA1: String get() = AppUtils.getAppSignatureSHA1()
 
+/**
+ * Return the signature of application for SHA1 value. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppSignatureSHA1(packageName)
+ * ```
+ */
 inline fun appSignatureSHA1Of(packageName: String): String =
   AppUtils.getAppSignatureSHA1(packageName)
 
-inline val appSignatureSHA256: String
-  get() = AppUtils.getAppSignatureSHA256()
+/**
+ * Return the signature of application for SHA256 value. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppSignatureSHA256()
+ * ```
+ */
+inline val appSignatureSHA256: String get() = AppUtils.getAppSignatureSHA256()
 
+/**
+ * Return the signature of application for SHA256 value. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppSignatureSHA256(packageName)
+ * ```
+ */
 inline fun appSignatureSHA256Of(packageName: String): String =
   AppUtils.getAppSignatureSHA256(packageName)
 
-inline val appSignatureMD5: String
-  get() = AppUtils.getAppSignatureMD5()
+/**
+ * Return the signature of application for MD5 value. This is equivalent to calling:
+ * ```
+ *  AppUtils.getAppSignatureMD5()
+ * ```
+ */
+inline val appSignatureMD5: String get() = AppUtils.getAppSignatureMD5()
 
-inline fun appSignatureMD5Of(packageName: String): String =
-  AppUtils.getAppSignatureMD5(packageName)
+/**
+ * Return the signature of application for MD5 value. This is equivalent to calling:
+ * ```
+ *  AppUtils.getAppSignatureMD5(packageName)
+ * ```
+ */
+inline fun appSignatureMD5Of(packageName: String): String = AppUtils.getAppSignatureMD5(packageName)
 
-inline val appInfo: AppUtils.AppInfo
-  get() = AppUtils.getAppInfo()
+/**
+ * Return the information of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppInfo()
+ * ```
+ */
+inline val appInfo: AppUtils.AppInfo get() = AppUtils.getAppInfo()
 
-inline fun appInfoOf(packageName: String): AppUtils.AppInfo =
-  AppUtils.getAppInfo(packageName)
+/**
+ * Return the information of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppInfo(packageName)
+ * ```
+ */
+inline fun appInfoOf(packageName: String): AppUtils.AppInfo = AppUtils.getAppInfo(packageName)
 
-inline val appsInfo: List<AppUtils.AppInfo>
-  get() = AppUtils.getAppsInfo()
+/**
+ * Return the information of applications. This is equivalent to calling:
+ * ```
+ * AppUtils.getAppsInfo()
+ * ```
+ */
+inline val appsInfo: List<AppUtils.AppInfo> get() = AppUtils.getAppsInfo()
 
-inline val File.apkInfo: AppUtils.AppInfo
-  get() = AppUtils.getApkInfo(this)
+/**
+ * Return the package information of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getApkInfo(apkFile)
+ * ```
+ */
+inline fun apkInfo(apkFile: File): AppUtils.AppInfo = AppUtils.getApkInfo(apkFile)
 
-inline val String.apkInfo: AppUtils.AppInfo
-  get() = AppUtils.getApkInfo(this)
+/**
+ * Return the package information of application. This is equivalent to calling:
+ * ```
+ * AppUtils.getApkInfo(apkFilePath)
+ * ```
+ */
+inline fun apkInfo(apkFilePath: String): AppUtils.AppInfo = AppUtils.getApkInfo(apkFilePath)
--- library/src/main/java/com/dylanc/utilktx/BarUtils.kt
+++ library/src/main/java/com/dylanc/utilktx/BarUtils.kt
@@ -16,32 +16,94 @@ import com.blankj.utilcode.util.BarUtils
  * @author Dylan Cai
  */
 
-inline val statusBarHeight: Int
-  get() = BarUtils.getStatusBarHeight()
+/**
+ * Returns the height of status bar. This is equivalent to calling:
+ * ```
+ * BarUtils.getStatusBarHeight()
+ * ```
+ */
+inline val statusBarHeight: Int get() = BarUtils.getStatusBarHeight()
 
+/**
+ * Returns whether the status bar is visible or sets the status bar to be visible.
+ * This is equivalent to calling:
+ * ```
+ * BarUtils.isStatusBarVisible(activity)
+ * ```
+ * or
+ * ```
+ * BarUtils.setStatusBarVisibility(activity, isVisible)
+ * ```
+ */
 inline var Activity.isStatusBarVisible: Boolean
   get() = BarUtils.isStatusBarVisible(this)
   set(value) = BarUtils.setStatusBarVisibility(this, value)
 
+/**
+ * Sets the status bar to be visible. This is equivalent to calling:
+ * ```
+ * BarUtils.setStatusBarVisibility(window, isVisible)
+ * ```
+ */
 inline var Window.isStatusBarVisible: Boolean
   @Deprecated("Property does not have a getter", level = DeprecationLevel.ERROR)
   get() = throw NotImplementedError()
   set(value) = BarUtils.setStatusBarVisibility(this, value)
 
+/**
+ * Returns whether the status bar is light mode or sets the status bar to be light mode.
+ * This is equivalent to calling:
+ * ```
+ * BarUtils.isStatusBarLightMode(activity)
+ * ```
+ * or
+ * ```
+ * BarUtils.setStatusBarLightMode(activity, isLightMode)
+ * ```
+ */
 inline var Activity.isStatusBarLightMode: Boolean
   get() = BarUtils.isStatusBarLightMode(this)
   set(value) = BarUtils.setStatusBarLightMode(this, value)
 
+/**
+ * Returns whether the status bar is light mode or sets the status bar to be light mode.
+ * This is equivalent to calling:
+ * ```
+ * BarUtils.isStatusBarLightMode(window)
+ * ```
+ * or
+ * ```
+ * BarUtils.setStatusBarLightMode(window, isLightMode)
+ * ```
+ */
 inline var Window.isStatusBarLightMode: Boolean
   get() = BarUtils.isStatusBarLightMode(this)
   set(value) = BarUtils.setStatusBarLightMode(this, value)
 
+/**
+ * Add the top margin size equals the height of status for view. This is equivalent to calling:
+ * ```
+ * BarUtils.addMarginTopEqualStatusBarHeight(view)
+ * ```
+ */
 inline fun View.addMarginTopEqualStatusBarHeight() =
   BarUtils.addMarginTopEqualStatusBarHeight(this)
 
+/**
+ * Subtract the top margin size equals the height of status for view. This is equivalent to calling:
+ * ```
+ * BarUtils.subtractMarginTopEqualStatusBarHeight(view)
+ * ```
+ */
 inline fun View.subtractMarginTopEqualStatusBarHeight() =
   BarUtils.subtractMarginTopEqualStatusBarHeight(this)
 
+/**
+ * Sets the color of status bar. This is equivalent to calling:
+ * ```
+ * BarUtils.setStatusBarColor(activity, color)
+ * ```
+ */
 var Activity.statusBarColor: Int
   @Deprecated("Property does not have a getter", level = DeprecationLevel.ERROR)
   get() = throw NotImplementedError()
@@ -49,63 +111,164 @@ var Activity.statusBarColor: Int
     BarUtils.setStatusBarColor(this, value)
   }
 
-inline fun Activity.setStatusBarColor(color: Int): View? =
-  BarUtils.setStatusBarColor(this, color)
-
+/**
+ * Sets the color of status bar. This is equivalent to calling:
+ * ```
+ * BarUtils.setStatusBarColor(window, color)
+ * ```
+ */
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 inline fun Window.setStatusBarColor(color: Int, transparentStatusBar: Boolean = true) {
   if (transparentStatusBar) BarUtils.setStatusBarColor(this, color) else statusBarColor = color
 }
 
+/**
+ * Sets the color of status bar for drawer layout. This is equivalent to calling:
+ * ```
+ * BarUtils.setStatusBarColor4Drawer(drawerLayout, fakeStatusBar, color, isTop)
+ * ```
+ */
 inline fun DrawerLayout.setStatusBarColor(fakeStatusBar: View, color: Int, isTop: Boolean = false) =
   BarUtils.setStatusBarColor4Drawer(this, fakeStatusBar, color, isTop)
 
-inline fun Activity.transparentStatusBar() =
-  BarUtils.transparentStatusBar(this)
+/**
+ * Sets the status bar transparent. This is equivalent to calling:
+ * ```
+ * BarUtils.transparentStatusBar(activity)
+ * ```
+ */
+inline fun Activity.transparentStatusBar() = BarUtils.transparentStatusBar(this)
 
-inline fun Window.transparentStatusBar() =
-  BarUtils.transparentStatusBar(this)
+/**
+ * Sets the status bar transparent. This is equivalent to calling:
+ * ```
+ * BarUtils.transparentStatusBar(window)
+ * ```
+ */
+inline fun Window.transparentStatusBar() = BarUtils.transparentStatusBar(this)
 
-inline val actionBarHeight: Int
-  get() = BarUtils.getActionBarHeight()
+/**
+ * Returns the height of action bar. This is equivalent to calling:
+ * ```
+ * BarUtils.getActionBarHeight()
+ * ```
+ */
+inline val actionBarHeight: Int get() = BarUtils.getActionBarHeight()
 
+/**
+ * Sets the notification bar to be visible. This is equivalent to calling:
+ * ```
+ * BarUtils.setNotificationBarVisibility(value)
+ * ```
+ */
 inline var isNotificationBarVisible: Boolean
   @Deprecated("Property does not have a getter", level = DeprecationLevel.ERROR)
   get() = throw NotImplementedError()
   @RequiresPermission(permission.EXPAND_STATUS_BAR)
   set(value) = BarUtils.setNotificationBarVisibility(value)
 
-inline val navBarHeight: Int
-  get() = BarUtils.getNavBarHeight()
+/**
+ * Returns the height of navigation bar. This is equivalent to calling:
+ * ```
+ * BarUtils.getNavBarHeight()
+ * ```
+ */
+inline val navBarHeight: Int get() = BarUtils.getNavBarHeight()
 
+/**
+ * Returns whether the navigation bar is visible or sets the navigation bar to be visible.
+ * This is equivalent to calling:
+ * ```
+ * BarUtils.isNavBarVisible(activity)
+ * ```
+ * or
+ * ```
+ * BarUtils.setNavBarVisibility(activity, isVisible)
+ * ```
+ */
 inline var Activity.isNavBarVisible: Boolean
   get() = BarUtils.isNavBarVisible(this)
   set(value) = BarUtils.setNavBarVisibility(this, value)
 
+/**
+ * Returns whether the navigation bar is visible. This is equivalent to calling:
+ * ```
+ * BarUtils.isNavBarVisible(window)
+ * ```
+ */
 inline var Window.isNavBarVisible: Boolean
   @Deprecated("Property does not have a getter", level = DeprecationLevel.ERROR)
   get() = throw NotImplementedError()
   set(value) = BarUtils.setNavBarVisibility(this, value)
 
+/**
+ * Returns the color of navigation bar or sets the color of navigation bar.
+ * This is equivalent to calling:
+ * ```
+ * BarUtils.getNavBarColor(activity)
+ * ```
+ * or
+ * ```
+ * BarUtils.setNavBarColor(activity, isVisible)
+ * ```
+ */
 inline var Activity.navBarColor: Int
   @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
   get() = BarUtils.getNavBarColor(this)
   @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
   set(value) = BarUtils.setNavBarColor(this, value)
 
+/**
+ * Returns the color of navigation bar or sets the color of navigation bar.
+ * This is equivalent to calling:
+ * ```
+ * BarUtils.getNavBarColor(window)
+ * ```
+ * or
+ * ```
+ * BarUtils.setNavBarColor(window, isVisible)
+ * ```
+ */
 inline var Window.navBarColor: Int
   @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
   get() = BarUtils.getNavBarColor(this)
   @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
   set(value) = BarUtils.setNavBarColor(this, value)
 
-inline val isSupportNavBar: Boolean
-  get() = BarUtils.isSupportNavBar()
+/**
+ * Returns whether the navigation bar is visible. This is equivalent to calling:
+ * ```
+ * BarUtils.isSupportNavBar()
+ * ```
+ */
+inline val isSupportNavBar: Boolean get() = BarUtils.isSupportNavBar()
 
+/**
+ * Returns whether the navigation bar is light mode or sets the navigation bar to be light mode.
+ * This is equivalent to calling:
+ * ```
+ * BarUtils.isNavBarLightMode(activity)
+ * ```
+ * or
+ * ```
+ * BarUtils.setNavBarLightMode(activity, isLightMode)
+ * ```
+ */
 inline var Activity.isNavBarLightMode: Boolean
   get() = BarUtils.isNavBarLightMode(this)
   set(value) = BarUtils.setNavBarLightMode(this, value)
 
+/**
+ * Returns whether the navigation bar is light mode or sets the navigation bar to be light mode.
+ * This is equivalent to calling:
+ * ```
+ * BarUtils.isNavBarLightMode(window)
+ * ```
+ * or
+ * ```
+ * BarUtils.setNavBarLightMode(window, isLightMode)
+ * ```
+ */
 inline var Window.isNavBarLightMode: Boolean
   get() = BarUtils.isNavBarLightMode(this)
   set(value) = BarUtils.setNavBarLightMode(this, value)
